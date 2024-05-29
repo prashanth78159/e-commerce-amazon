8f877ae188e00b02122f40dd207a320d4b6902ae
prash_12 <plavishetti@gmail.com>
2024-05-29 14:08:26 +0530

    ok

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions,getDeliverOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 const today = dayjs();
 const defaultDeliveryDate = today.add(7, 'days');
@@ -102,6 +103,8 @@ export function renderOrderSummary(){
             removeFromCart(productId);
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
+
+            renderPaymentSummary();
         });
     });
 
@@ -111,6 +114,7 @@ export function renderOrderSummary(){
                 const {productId,deliveryOptionId}=element.dataset;
                 updateDeliveryOption(productId, deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             });
         });
 }

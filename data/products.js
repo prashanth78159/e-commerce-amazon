2fff66cc4f1b7582dac7120ca4680a3e95cd0fce
prash_12 <plavishetti@gmail.com>
2024-05-30 16:28:10 +0530

    ok

--- data/products.js
+++ data/products.js
@@ -29,23 +29,7 @@ class Product{
   }
 }
 
-const product1=new Product({
-  
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
 
-});
 
 export const products = [
   {
@@ -709,4 +693,3 @@ export const products = [
 ].map((productDetails) => {
   return new Product(productDetails);
 });
-console.log(products);

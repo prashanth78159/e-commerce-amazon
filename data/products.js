7753a678fa3cfde7ae4b88caae0d546884896037
prash_12 <plavishetti@gmail.com>
2024-06-01 17:00:51 +0530

    ok

--- data/products.js
+++ data/products.js
@@ -49,7 +49,28 @@ class Clothing extends Product{
   }
 }
 
+export let products=[];
 
+export function loadProducts(fun){
+  const xhr=new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products=JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type==='clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+/*
 
 export const products = [
   {
@@ -716,3 +737,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,8 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-renderOrderSummary();
-renderPaymentSummary();
+import { loadProducts } from "../data/products.js";
+
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
